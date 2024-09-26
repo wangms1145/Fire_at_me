d3b1c3a70c4488984c890b8ac0ffdc060e5d6cdc
wangms1145 <wangms1145@gmail.com>
2024-09-26 00:16:43 -0700

    exiting recoils guys
    EZ recoil done!!!!!!!

---
+++ Assets/Cam_script.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Cam_script : MonoBehaviour
+{
+    private Camera cam;
+    public Vector2 mousePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnGUI()
+    {
+        Vector3 point = new Vector3();
+        Event   currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        mousePosition.x = point.x;
+        mousePosition.y = point.y;
+        GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+        GUILayout.Label("Screen pixels: " + cam.pixelWidth + ":" + cam.pixelHeight);
+        GUILayout.Label("Mouse position: " + mousePos);
+        GUILayout.Label("World position: " + point.ToString("F3"));
+        GUILayout.EndArea();
+    }
+}
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+using System;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -9,12 +14,17 @@ public class PlayerScript : MonoBehaviour
     public float acc;
     public float aacc;
     public float jumpStrength;
+    public float recoil;
+    public Cam_script cam_script;
+    private float ang;
     private float spdx,spdy;
     private float tspd;
+    private float mouY,mouX;
     // Start is called before the first frame update
     void Start()
     {
         //myRigidbody.freezeRotation = true;
+        //transform.position.y;
     }
 
     // Update is called once per frame
@@ -23,6 +33,19 @@ public class PlayerScript : MonoBehaviour
         spdx = myRigidbody.velocity.x;
         spdy = myRigidbody.velocity.y;
         if(Input.GetKeyDown(KeyCode.Space))myRigidbody.velocity += Vector2.up * jumpStrength;
+        mouX = cam_script.mousePosition.x;
+        mouY = cam_script.mousePosition.y;
+        ang = Mathf.Atan((mouY - transform.position.y)/(mouX - transform.position.x));
+        //Debug.LogWarning((mouY - transform.position.y) + " " + (mouX - transform.position.x));
+        //ang/=Mathf.PI;
+        //ang*=180;
+        if(mouX - transform.position.x > 0){
+            ang+=Mathf.PI;
+        }
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            myRigidbody.velocity += Vector2.right * (Mathf.Cos(ang) * recoil);
+            myRigidbody.velocity += Vector2.up * (Mathf.Sin(ang) * recoil);
+        }
         tspd = 0;
         if(Input.GetKey(KeyCode.A))tspd += spd;
         if(Input.GetKey(KeyCode.D))tspd -= spd;
