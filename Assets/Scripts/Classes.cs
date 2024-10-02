28bb43bdc396e382447c6bb60ff8cb14336a7e9a
wangms1145 <wangms1145@gmail.com>
2024-10-01 21:13:49 -0700

    wired bug
    wired bug

--- Assets/Scripts/Classes.cs
+++ Assets/Scripts/Classes.cs
@@ -18,12 +18,17 @@ public class Classes : ParentClass
 public class WeaponClass
 {
     Sprite spr;
+    string name;
     float recoil;
     float bulletSpd;
     float maxDistance;
     int type;
     Vector2 firePos;
     Sprite fireEff;
+    int damageType;
+    float damage;
+    float firing_time;
+    float reload_time;
 }
 public class Bullets
 {
@@ -33,5 +38,7 @@ public class Bullets
     float maxDistance;
     float trigDistance;
     int type;
+    int damageType;
+    float damage;
 }
 
