4305a31833e022cf6708158157461da26b9a84d0
wangms1145 <wangms1145@gmail.com>
2024-10-07 15:55:25 -0700

    NOVA
    NOVA

--- Assets/Scripts/Bullet_gene_scr.cs
+++ Assets/Scripts/Bullet_gene_scr.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Rendering;
 public class Bullet_gene_scr : MonoBehaviour
@@ -22,10 +23,20 @@ public class Bullet_gene_scr : MonoBehaviour
     public void SpawnBullet(WeaponClass wp,float ang){
         //need spawn bullet
         Debug.Log("spawn");
-        GameObject bullet = bullets[wp.damageType];
+        GameObject bullet = bullets[wp.type];
         transform.localPosition = wp.firePos;
-        //bullet.GetComponent<Rigidbody2D>().velocity = (float)(Math.Cos(ang) * wp.bulletSpd) * Vector2.right + (float)(Math.Sin(ang) * wp.bulletSpd) * Vector2.up;
-        GameObject bullet_spawned = Instantiate(bullet, transform.position, quaternion.RotateZ(ang));
-        bullet_spawned.GetComponent<Rigidbody2D>().velocity = (float)(Math.Cos(ang) * wp.bulletSpd) * Vector2.right + (float)(Math.Sin(ang) * wp.bulletSpd) * Vector2.up;
+        if(wp.type == 1){
+            for (int i = 1; i <= UnityEngine.Random.Range(8,12); i++){
+                GameObject bullet_spawned = Instantiate(bullet, transform.position, quaternion.RotateZ(ang));
+                float angp = (float)(UnityEngine.Random.Range(-2,2)/180.0 * Math.PI);
+                float spdp = (float)(UnityEngine.Random.Range(-3, 3) / 3.0);
+                bullet_spawned.GetComponent<Rigidbody2D>().velocity = (float)(Math.Cos(ang+angp) * (wp.bulletSpd+spdp)) * Vector2.right + (float)(Math.Sin(ang+angp) * (wp.bulletSpd+spdp)) * Vector2.up;
+            }
+        }
+        else{
+            GameObject bullet_spawned = Instantiate(bullet, transform.position, quaternion.RotateZ(ang));
+            bullet_spawned.GetComponent<Rigidbody2D>().velocity = (float)(Math.Cos(ang) * wp.bulletSpd) * Vector2.right + (float)(Math.Sin(ang) * wp.bulletSpd) * Vector2.up;
+        }
+        
     }
 }
