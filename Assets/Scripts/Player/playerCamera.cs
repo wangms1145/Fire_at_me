17e79895ce12536ee03eefe7f701498a4122f28e
wangms1145 <wangms1145@gmail.com>
2024-12-04 16:09:32 -0800

    camera
    camera

--- Assets/Scripts/Player/playerCamera.cs
+++ Assets/Scripts/Player/playerCamera.cs
@@ -16,17 +16,19 @@ public class playerCamera : NetworkBehaviour
             cam_script = cam.GetComponent<Cam_script>();
             GameObject vc = GameObject.Find("VC player camera");
             vc.GetComponent<VC_Script>().ply = GetComponent<PlayerScript>();
-            vc.GetComponent<VC_Script>().myVCam.Follow = transform.GetChild(1);
+            vc.GetComponent<VC_Script>().myVCam.Follow = transform.GetChild(3);
             GameObject aim = GameObject.FindGameObjectWithTag("Aim");
             aim.GetComponent<debug>().ply = GetComponent<PlayerScript>();
         }
     }
     public void cam(){
-        varibles.mouX = cam_script.mousePosition.x;
-        varibles.mouY = cam_script.mousePosition.y;
-        varibles.sX = transform.position.x;
-        varibles.sY = transform.position.y;
-        varibles.disY = (varibles.mouY - transform.position.y) * varibles.mouse_mult;
-        varibles.disX = (varibles.mouX - transform.position.x) * varibles.mouse_mult;
+        if(IsOwner){
+            varibles.mouX = cam_script.mousePosition.x;
+            varibles.mouY = cam_script.mousePosition.y;
+            varibles.sX = transform.position.x;
+            varibles.sY = transform.position.y;
+            varibles.disY = (varibles.mouY - transform.position.y) * varibles.mouse_mult;
+            varibles.disX = (varibles.mouX - transform.position.x) * varibles.mouse_mult;
+        }
     }
 }
