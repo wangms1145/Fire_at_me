d2d0ee3d347450892cec8bee3b3b89a0851edbfb
COOKIES114 <snailmen666@gmail.com>
2025-01-23 16:55:02 -0800

    font
    font

---
+++ Assets/Scripts/UI/txt_script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class txt_script : MonoBehaviour
+{
+    //public Text scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //scoretext = GetComponentInChildren<Text>()[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
