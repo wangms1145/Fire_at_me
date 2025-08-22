948f41d10269da338de496c71c4f7b84956e800e
yayyayyayyayyay <ax55514@pausd.us>
2025-08-22 15:24:04 -0700

    西瓜味炸鸡
    ni

--- Assets/Scripts/Player/playerRecoil.cs
+++ Assets/Scripts/Player/playerRecoil.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
+//One instance of this class represents logic for player recoil
 public class playerRecoil : NetworkBehaviour
 {
     public PlayerScript varibles;
@@ -15,45 +16,52 @@ public class playerRecoil : NetworkBehaviour
     public float up_comp_factor;
     public float up_comp_factor_recoil;
     private NetworkVariable<Vector2> speedNet = new NetworkVariable<Vector2>(
-                                                    new Vector2(0,0),
+                                                    new Vector2(0, 0),
                                                     NetworkVariableReadPermission.Everyone,
                                                     NetworkVariableWritePermission.Server
                                                     );
     private float recoil;
     //public bool heavy = false;
-    void Start(){
+    void Start()
+    {
         varibles = GetComponent<PlayerScript>();
         myRigidbody = GetComponent<Rigidbody2D>();
         player_logic = GetComponent<playerLogic>();
         player_move = GetComponent<playerMove>();
         //heavy_time = heavy_time_max;
     }
-    public void player_recoil(){
-        if(varibles.shoot){
+    public void player_recoil()
+    {
+        if (varibles.shoot)
+        {
             recoil = varibles.recoil;
-            if(player_logic.heavy)recoil /= 10;
+            if (player_logic.heavy) recoil /= 10;
             addSpd(Vector2.right * (Mathf.Cos(varibles.ang) * recoil));
             float up_recoil = Mathf.Sin(varibles.ang) * recoil;
             addSpd(Vector2.up * up_recoil);
-            if(speedNet.Value.y < 0){
+            if (speedNet.Value.y < 0)
+            {
                 addSpd(Vector2.up * Mathf.Abs(speedNet.Value.y) * up_comp_factor);
-                if(up_recoil > 0){
+                if (up_recoil > 0)
+                {
                     addSpd(Vector2.up * up_recoil * up_comp_factor_recoil);
                 }
             }
             varibles.time_last_shoot = Time.time;
-            wp.recoil_ani = (float)Math.Clamp(varibles.recoil / 6.0,0,0.7);
+            wp.recoil_ani = (float)Math.Clamp(varibles.recoil / 6.0, 0, 0.7);
             varibles.shoot = false;
         }
     }
     void Update()
     {
-        if(IsServer){
+        if (IsServer)
+        {
             speedNet.Value = myRigidbody.velocity;
         }
     }
-    private void addSpd(Vector2 spd){
+    private void addSpd(Vector2 spd)
+    {
         player_move.addSpdRPC(spd);
     }
-    
+
 }
